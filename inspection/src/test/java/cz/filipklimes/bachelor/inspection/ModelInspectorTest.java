8717c65063d94639ff6cf208f54f25bcc85d4dad
Filip Klimes <filip@filipklimes.cz>
2015-03-01 12:43:02 +0100

    Maven: added parent project

--- inspection/src/test/java/cz/filipklimes/bachelor/inspection/ModelInspectorTest.java
+++ inspection/src/test/java/cz/filipklimes/bachelor/inspection/ModelInspectorTest.java
@@ -1,7 +1,6 @@
 package cz.filipklimes.bachelor.inspection;
 
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
